a4f513bf81d4fd2828d4e027188b665c8bd2daba
Hernando Bifaretti <hbifaretti@gmail.com>
2022-07-21 10:17:17 +0200

    home-page-css

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -37,7 +37,7 @@ function IndividualIntervalsExample() {
           alt="Third slide"
         />
         <Carousel.Caption>
-          <h3>Third slide label</h3>
+          <h3>hola yow</h3>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
